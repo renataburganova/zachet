e8b701a194dc7b6d19f32fa2c4eb057aaa5259d7
Burganova Renata <st117269@student.spbu.ru>
2024-08-06 19:12:41 +0300

    [vector] Implement tests for the insert and remove_first vector methods

--- test/vector/test_vector.cpp
+++ test/vector/test_vector.cpp
@@ -19,6 +19,33 @@ TEST(push_back, full) {
 	EXPECT_FALSE(v.has_item(4));
 }
 
+TEST(insert, full) {
+	Vector<int> v;
+	v.push_back(1);
+	v.push_back(3);
+	v.push_back(4);
+	v.insert(1, 2);
+	ASSERT_EQ(4, v.get_size());
+	EXPECT_TRUE(v.has_item(2));
+}
+
+TEST(remove_first, full) {
+	Vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(3);
+	ASSERT_EQ(4, v.get_size());
+
+	v.remove_first(3);
+	ASSERT_EQ(3, v.get_size());
+	EXPECT_TRUE(v.has_item(3));
+
+	v.remove_first(3);
+	ASSERT_EQ(2, v.get_size());
+	EXPECT_FALSE(v.has_item(3));
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
